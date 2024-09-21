c1c3e5d78e45f794ffbed472c48ef0ce70a33492
Manokamna Singh <85088233+Manokamnasingh1@users.noreply.github.com>
2024-09-21 09:45:56 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+
+function App() {
+    return (
+        <div>
+            <h1>Contact Form</h1>
+            <Form />
+        </div>
+    );
+}
+
+export default App;
